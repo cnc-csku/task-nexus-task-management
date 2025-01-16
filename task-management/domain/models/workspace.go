f248cdb80095db04cddf6fda3c6758486ef56a61
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2025-01-16 14:59:34 +0700

    add workspace model

---
+++ task-management/domain/models/workspace.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Workspace struct {
+	ID        bson.ObjectID     `bson:"_id,omitempty" json:"id"`
+	Name      string            `bson:"name" json:"name"`
+	Members   []WorkspaceMember `bson:"members" json:"members"`
+	CreatedBy bson.ObjectID     `bson:"created_by" json:"createdBy"`
+	CreatedAt time.Time         `bson:"created_at" json:"createdAt"`
+	UpdatedAt time.Time         `bson:"updated_at" json:"updatedAt"`
+}
+
+type WorkspaceMember struct {
+	UserID    bson.ObjectID       `bson:"user_id" json:"userId"`
+	Name      string              `bson:"name" json:"name"`
+	Role      WorkspaceMemberRole `bson:"role" json:"role"`
+	JoinedAt  time.Time           `bson:"joined_at" json:"joinedAt"`
+	RemovedAt *time.Time          `bson:"removed_at,omitempty" json:"removedAt,omitempty"`
+}
+
+type WorkspaceMemberRole string
+
+const (
+	WorkspaceMemberRoleAdmin WorkspaceMemberRole = "ADMIN"
+	WorkspaceMemberRoleUser  WorkspaceMemberRole = "MEMBER"
+)
+
+func (w WorkspaceMemberRole) String() string {
+	return string(w)
+}
+
+func (w WorkspaceMemberRole) IsValid() bool {
+	switch w {
+	case WorkspaceMemberRoleAdmin, WorkspaceMemberRoleUser:
+		return true
+	}
+	return false
+}
